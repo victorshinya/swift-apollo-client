81c231daadb0f6f432f03fc94162ec4684ea9b45
Victor Shinya <contato@victorshinya.com.br>
2019-12-09 18:55:43 -0300

    Initial Commit

---
+++ Swift Apollo Client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Apollo Client
+//
+//  Created by Victor Shinya on 09/12/19.
+//  Copyright © 2019 Victor Shinya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
